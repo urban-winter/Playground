7d4ecb599048882a4a3fbe48c06213d8b7d780ba
Piers Thompson <piers.thompson@baml.com>
2015-06-25 09:13:00 +0100

    playing with hypothesis - merging sorted lists

--- mergetest.py
+++ mergetest.py
@@ -2,11 +2,7 @@ from hypothesis import given
 from hypothesis.strategies import lists, integers, builds
 
 def pop_least(list1,list2):
-    """return the smaller element from two ordered lists, and remove it
-    
-    If an element is the same in both lists then it is returned and removed
-    from both list which effectively deduplicates when called successively
-    to merge two ordered lists.
+    """return the smaller element from one of two ordered lists, and remove it
     """
     if list1[0] <= list2[0]:
         return list1.pop(0)
@@ -15,13 +11,9 @@ def pop_least(list1,list2):
 def merge_sorted_lists(sorted_list_1, sorted_list_2):
     """merge two ascending lists"""
     retlist = []
-    while sorted_list_1 or sorted_list_2:
-        if not sorted_list_1:
-            retlist.extend(sorted_list_2)
-        elif not sorted_list_2:
-            retlist.extend(sorted_list_1)
-        else:
-            retlist.append(pop_least(sorted_list_1,sorted_list_2))
+    while sorted_list_1 and sorted_list_2:
+        retlist.append(pop_least(sorted_list_1,sorted_list_2))
+    retlist.extend(sorted_list_1 if sorted_list_1 else sorted_list_2)
     return retlist
 
 def sort_list(l):
